a771123c65a4ec72aebcbabfdab6004dd8302d11
Christian González <121120049+MemoHerdez4@users.noreply.github.com>
2022-12-20 15:36:03 -0600

    Add files via upload

---
+++ Operadores.java
@@ -0,0 +1,51 @@
+public class Operadores {
+    public static void main(String[] args) {
+        // Operador Arimetico
+        int number1 = 5;
+        int number2 = 5;
+
+        int Sumar = number1 + number2; // El Operador "+" ayuda a sumar una variable 1 + una variable 2
+        int Restar = number1 + number2; // El Operador "-" ayuda a restar una variable 1 - una variable 2
+        int Multiplicar = number1 * number2; // El Operador "*" ayuda a restar una variable 1 * una variable 2
+        int Dividir = number1 / number2; // El Operador "/" ayuda a restar una variable 1 / una variable 2
+        
+        // Operadores Logicos, Relación, Comparación, Booleanos
+        
+        /*
+
+        > Indica si un numero solo es mayor a otro numero
+        >= Indica si un numero es mayor o igual a otro numero
+        < Indica si un numero es menor a otro numero
+        <= Indica si un numero es menor o igual a otro numero
+        == Indica si un numero es igual a otro numero
+        != Indica si un numero es diferente a otro numero
+        && and Indica si se cumple 2 condiciones a la vez
+        || or Indica si se cumple esto u esto
+        !true Indica un False
+
+        */
+
+        // Operador de Asignación
+        /*
+        = Asignas a una variable a algo 
+        += Puedes combinarlo con una suma
+        -= Puedes combinarlo con una resta
+        /= Puedes combinarlo con una división
+        *= Puedes combinarlo con una multiplicación
+        %= Puedes combinarlo con una para saber si es divisible
+         */
+
+        // Operador Incremento
+        
+        // ++ Incrementas un valor
+
+        // Operador Decremento
+        
+        // -- Decrementas un valor
+        
+        // Operador Concatenación
+
+        // + Juntas cadenas de texto con otras        
+
+    }    
+}
---
+++ Tipos.java
@@ -0,0 +1,29 @@
+public class Tipos {
+    public static void main(String[] args) {
+        //Enteros
+        byte number1 = 1; // 1 Byte
+        short number2 = 2; // 2 byte
+        int number3 = 3; // 4 Byte
+        long number4 = 4; // 8 Byte
+
+        //Flotante
+        float decimal1 = 4.9f; // f para indicar que es un flotante
+        double decimal2 = 9.99d; // d para indicar que es un Double
+
+        //Caracter
+        char caracter1 = 'a'; // Comilla simple '' == Caracter
+
+        //Cadena de Texto
+        String Nombre = "Christian"; // Comilla Doble "" == Cadena de texto
+        String Apellido = "González"; // No es lo mismo que la comilla simple, ya que tienen mas palabras
+
+        //Boolean
+        boolean verdadero = true; // Adquieren un color azul
+        boolean falso = false; // Son palabras reservadas
+
+        //Tipo Envoltorio
+        Integer numero = null; // Si se usa un int no se podra convertir en un atributo opcional como Null
+        Long numero2 = 2L; //
+        
+    }
+}
